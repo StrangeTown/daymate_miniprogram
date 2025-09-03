300bbec365a901d0dca449f4f694ac3efe7b8d84
Xing <yinxingdyx@163.com>
2025-09-03 17:03:43 +0800

    Refactor event images: reorganize categories and update assets

--- constants/index.js
+++ constants/index.js
@@ -1,94 +1,20 @@
 // the name of event images should be less then 16 characters
 const EVENT_IMAGES = [
-  // book.png
+  // === EDUCATION & LEARNING ===
   {
     name: 'book',
     localAddress: "assets/images/event_images/book.png"
   },
-  // business.png
   {
-    name: 'business',
-    localAddress: "assets/images/event_images/business.png"
+    name: 'graduation_hat',
+    localAddress: "assets/images/event_images/graduation_hat.png"
   },
-  // check.png
   {
-    name: 'check',
-    localAddress: "assets/images/event_images/check.png"
-  },
-  // dove.png
-  {
-    name: 'dove',
-    localAddress: "assets/images/event_images/dove.png"
-  },
-  // gift.png
-  {
-    name: 'gift',
-    localAddress: "assets/images/event_images/gift.png"
-  },
-  // move.png
-  {
-    name: 'move',
-    localAddress: "assets/images/event_images/move.png"
-  },
-  // ski.png
-  {
-    name: 'ski',
-    localAddress: "assets/images/event_images/ski.png"
-  },
-  // travel.png
-  {
-    name: 'travel',
-    localAddress: "assets/images/event_images/travel.png"
-  },
-  // baby.png
-  {
-    name: 'baby',
-    localAddress: "assets/images/event_images/baby.png"
-  },
-  // basketball_game.png
-  {
-    name: 'basketball_game',
-    localAddress: "assets/images/event_images/basketball_game.png"
-  },
-  // cake.png
-  {
-    name: 'birthday_cake',
-    localAddress: "assets/images/event_images/birthday_cake.png"
-  },
-  // dog.png
-  {
-    name: 'dog',
-    localAddress: "assets/images/event_images/dog.png"
-  },
-  // fireworks.png
-  {
-    name: 'fireworks',
-    localAddress: "assets/images/event_images/fireworks.png"
-  },
-  // microphone.png
-  {
-    name: 'microphone',
-    localAddress: "assets/images/event_images/microphone.png"
-  },
-  // rank.png
-  {
-    name: 'rank',
-    localAddress: "assets/images/event_images/rank.png"
-  },
-  // wine.png
-  {
-    name: 'wine',
-    localAddress: "assets/images/event_images/wine.png"
+    name: 'school',
+    localAddress: "assets/images/event_images/2125344_education_learn_school_student_study_icon.svg"
   },
 
-  {
-    name: 'turkey',
-    localAddress: "assets/images/event_images/turkey.png"
-  },
-  {
-    name: 'shopping_cart',
-    localAddress: "assets/images/event_images/3507742_cart_ecommerce_grocery_iconoteka_shop_icon.svg"
-  },
+  // === TRAVEL & LANDMARKS ===
   {
     name: 'eiffel_tower',
     localAddress: "assets/images/event_images/3465581_architecture_eiffel_france_landmark_paris_icon.svg"
@@ -122,76 +48,36 @@ const EVENT_IMAGES = [
     localAddress: "assets/images/event_images/3465592_fuji_japan_landmark_landscape_mountain_icon.svg"
   },
 	{
+    name: 'japan',
+    localAddress: "assets/images/event_images/japan-svgrepo-com.svg"
+  },
+	{
+    name: 'egypt',
+    localAddress: "assets/images/event_images/egypt.png"
+  },
+
+  // === FOOD & DRINK ===
+	{
     name: 'apple',
     localAddress: "assets/images/event_images/apple-svgrepo-com.svg"
-  },
-	{
-    name: 'baby_carriage',
-    localAddress: "assets/images/event_images/baby_carriage_solid_icon.svg"
-  },
-	{
-    name: 'christmas_tree',
-    localAddress: "assets/images/event_images/birthday-celebration-christmas-festival-party-2-svgrepo-com.svg"
   },
 	{
     name: 'pizza',
     localAddress: "assets/images/event_images/birthday_celebration_food_party_pizza_icon.png"
   },
 	{
-    name: 'building_estate',
-    localAddress: "assets/images/event_images/building_estate_home_house_property_icon.svg"
+    name: 'turkey',
+    localAddress: "assets/images/event_images/turkey.png"
   },
-	{
-    name: 'hospital',
-    localAddress: "assets/images/event_images/hospital.png"
+  {
+    name: 'wine',
+    localAddress: "assets/images/event_images/wine.png"
   },
-	{
-    name: 'cherry_blossom',
-    localAddress: "assets/images/event_images/cherry-blossom-spring-svgrepo-com.svg"
-  },
-	{
-    name: 'crystal',
-    localAddress: "assets/images/event_images/crystal_pentagon_diamond_gemstone_jewelry_icon.svg"
-  },
-	{
-    name: 'edit_pen',
-    localAddress: "assets/images/event_images/edit_pen_pencil_write_writing_icon.svg"
-  },
-	{
-    name: 'egypt',
-    localAddress: "assets/images/event_images/egypt.png"
-  },
-	{
-    name: 'fighter_jet',
-    localAddress: "assets/images/event_images/fighter-jet-solid-svgrepo-com.svg"
-  },
-	{
-    name: 'graduation_hat',
-    localAddress: "assets/images/event_images/graduation_hat.png"
-  },
-	{
-    name: 'japan',
-    localAddress: "assets/images/event_images/japan-svgrepo-com.svg"
-  },
-	{
-    name: 'movies',
-    localAddress: "assets/images/event_images/movies_music_videos_icon.svg"
-  },
-	{
-    name: 'balloons',
-    localAddress: "assets/images/event_images/new_year_balloons_celebration_decoration_icon.svg"
-  },
-	{
-    name: 'palette',
-    localAddress: "assets/images/event_images/palette.png"
-  },
-	{
-    name: 'palm',
-    localAddress: "assets/images/event_images/palm.png"
-  },
-	{
-    name: 'podcast',
-    localAddress: "assets/images/event_images/podcast-svgrepo-com.svg"
+
+  // === SPORTS & GAMES ===
+  {
+    name: 'basketball_game',
+    localAddress: "assets/images/event_images/basketball_game.png"
   },
 	{
     name: 'basketball',
@@ -213,17 +99,135 @@ const EVENT_IMAGES = [
     name: 'tennis',
     localAddress: "assets/images/event_images/tennis.png"
   },
+
+  // === CELEBRATION & PARTY ===
+  {
+    name: 'birthday_cake',
+    localAddress: "assets/images/event_images/birthday_cake.png"
+  },
 	{
-    name: 'ticket',
-    localAddress: "assets/images/event_images/ticket_light_icon.svg"
+    name: 'christmas_tree',
+    localAddress: "assets/images/event_images/birthday-celebration-christmas-festival-party-2-svgrepo-com.svg"
+  },
+	{
+    name: 'fireworks',
+    localAddress: "assets/images/event_images/fireworks.png"
+  },
+	{
+    name: 'balloons',
+    localAddress: "assets/images/event_images/new_year_balloons_celebration_decoration_icon.svg"
   },
 	{
     name: 'valentines',
     localAddress: "assets/images/event_images/valentines_rose_heart_love_lover_icon.svg"
   },
+
+  // === BUSINESS & WORK ===
+  {
+    name: 'business',
+    localAddress: "assets/images/event_images/business.png"
+  },
+  {
+    name: 'check',
+    localAddress: "assets/images/event_images/check.png"
+  },
+  {
+    name: 'rank',
+    localAddress: "assets/images/event_images/rank.png"
+  },
+
+  // === FAMILY & PERSONAL ===
+  {
+    name: 'baby',
+    localAddress: "assets/images/event_images/baby.png"
+  },
 	{
-    name: 'school',
-    localAddress: "assets/images/event_images/2125344_education_learn_school_student_study_icon.svg"
+    name: 'baby_carriage',
+    localAddress: "assets/images/event_images/baby_carriage_solid_icon.svg"
+  },
+  {
+    name: 'dog',
+    localAddress: "assets/images/event_images/dog.png"
+  },
+
+  // === HEALTH & MEDICAL ===
+	{
+    name: 'hospital',
+    localAddress: "assets/images/event_images/hospital.png"
+  },
+
+  // === NATURE & OUTDOOR ===
+	{
+    name: 'cherry_blossom',
+    localAddress: "assets/images/event_images/cherry-blossom-spring-svgrepo-com.svg"
+  },
+	{
+    name: 'palm',
+    localAddress: "assets/images/event_images/palm.png"
+  },
+  {
+    name: 'ski',
+    localAddress: "assets/images/event_images/ski.png"
+  },
+
+  // === ARTS & CREATIVE ===
+	{
+    name: 'palette',
+    localAddress: "assets/images/event_images/palette.png"
+  },
+  {
+    name: 'microphone',
+    localAddress: "assets/images/event_images/microphone.png"
+  },
+	{
+    name: 'podcast',
+    localAddress: "assets/images/event_images/podcast-svgrepo-com.svg"
+  },
+	{
+    name: 'movies',
+    localAddress: "assets/images/event_images/movies_music_videos_icon.svg"
+  },
+
+  // === SHOPPING & COMMERCE ===
+  {
+    name: 'shopping_cart',
+    localAddress: "assets/images/event_images/3507742_cart_ecommerce_grocery_iconoteka_shop_icon.svg"
+  },
+
+  // === TRANSPORTATION ===
+  {
+    name: 'move',
+    localAddress: "assets/images/event_images/move.png"
+  },
+	{
+    name: 'fighter_jet',
+    localAddress: "assets/images/event_images/fighter-jet-solid-svgrepo-com.svg"
+  },
+
+  // === SYMBOLS & OBJECTS ===
+  {
+    name: 'dove',
+    localAddress: "assets/images/event_images/dove.png"
+  },
+  {
+    name: 'gift',
+    localAddress: "assets/images/event_images/gift.png"
+  },
+	{
+    name: 'crystal',
+    localAddress: "assets/images/event_images/crystal_pentagon_diamond_gemstone_jewelry_icon.svg"
+  },
+	{
+    name: 'edit_pen',
+    localAddress: "assets/images/event_images/edit_pen_pencil_write_writing_icon.svg"
+  },
+	{
+    name: 'ticket',
+    localAddress: "assets/images/event_images/ticket_light_icon.svg"
+  },
+	{
+    name: 'building_estate',
+    localAddress: "assets/images/event_images/building_estate_home_house_property_icon.svg"
   },
 ];
 
